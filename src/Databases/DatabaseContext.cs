c838a41b1d27c99ff3908abcb94b9282b6b7be78
Bashayr <bashayerali.al-wahbi@integrify.io>
2024-05-02 16:02:56 +0300

    errors fixed

--- src/Databases/DatabaseContext.cs
+++ src/Databases/DatabaseContext.cs
@@ -11,7 +11,7 @@ namespace CodeCrafters_backend_teamwork.src.Databases;
 public class DatabaseContext
 {
     public IEnumerable<Product> products { get; }
-    public IEnumerable<User> uesers { get; }
+    public IEnumerable<User> users { get; }
     public IEnumerable<Stock> stocks { get; }
 
     public DatabaseContext()
@@ -19,16 +19,18 @@ public class DatabaseContext
         products = [
             new Product("bag", "pic1", 44.5),
              new Product("purse", "pic2", 44.5),
-             new Product("shirt", "pic3", 44.5),];
+             new Product("shirt", "pic3", 44.5)
+             ];
 
         stocks = [
-            new Stock("bag", "pic1", "44.5")];
+            new Stock("bag", "pic1", "44.5")
+            ];
 
         users = [
-            new User( "Aishah", "AlSaud" , "123456" , "Aishah@gmail.com", "0544444444"),
-            new User(  "Renad", "AlZahrani", "654321",  "Renad@gmail.com" ,"0533333344"),
-            new User( "Raneem", "AlMutairi", "098765", "Raneem@gmail.com", "0555555544"),
-            new User( "Bashayer", "AlWahbi", "567890", "Bashayer@gmail.com", "0566666644"),
+            new User(12, "Aishah", "AlSaud" , "123456" , "Aishah@gmail.com", 0544444444),
+            new User(12,  "Renad", "AlZahrani", "654321",  "Renad@gmail.com", 0533333344),
+            new User(12, "Raneem", "AlMutairi", "098765", "Raneem@gmail.com", 0555555544),
+            new User(12,  "Bashayer", "AlWahbi", "567890", "Bashayer@gmail.com", 0566666644)
 
         ];
     }
--- src/Repositories/OrderItemRepository.cs
+++ src/Repositories/OrderItemRepository.cs
@@ -1,53 +1,53 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using CodeCrafters_backend_teamwork.src.Controllers;
-using CodeCrafters_backend_teamwork.src.Databases;
-using CodeCrafters_backend_teamwork.src.Entities;
-using CodeCrafters_backend_teamwork.src.Service;
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Threading.Tasks;
+// using CodeCrafters_backend_teamwork.src.Controllers;
+// using CodeCrafters_backend_teamwork.src.Databases;
+// using CodeCrafters_backend_teamwork.src.Entities;
+// using CodeCrafters_backend_teamwork.src.Service;
 
-namespace CodeCrafters_backend_teamwork.src.Repository
-{
-   public class OrderItemRepository : IOrderItemRepository
-    {
-        IEnumerable<OrderItem> Orderitem;
+// namespace CodeCrafters_backend_teamwork.src.Repository
+// {
+//    public class OrderItemRepository : IOrderItemRepository
+//     {
+//         IEnumerable<OrderItem> Orderitem;
 
-        public OrderItemRepository()
-        {
-            orderitem = new DatabaseContext().OrderItem;
+//         public OrderItemRepository()
+//         {
+//             orderitem = new DatabaseContext().OrderItem;
 
-        }
-        public IEnumerable<OrderItem> FindAll()
-        {
-            return orderitem;
-        }
-        public OrderItem FindOne(OrderItem orderitem)
-        {
-            return orderitem;
-        }
-        public OrderItem CreateOne(OrderItem orderitem)
-        {
-            return orderitem;
-        }
-        public OrderItem UpdateOne(OrderItem orderitem)
-        {
-            return orderitem;
-        }
-        public IEnumerable<OrderItem> DeleteAll(string id)
-    {
-        orderitem.Where(orderitem => orderitem.Id == id);
-        return orderitem;
-    }
+//         }
+//         public IEnumerable<OrderItem> FindAll()
+//         {
+//             return orderitem;
+//         }
+//         public OrderItem FindOne(OrderItem orderitem)
+//         {
+//             return orderitem;
+//         }
+//         public OrderItem CreateOne(OrderItem orderitem)
+//         {
+//             return orderitem;
+//         }
+//         public OrderItem UpdateOne(OrderItem orderitem)
+//         {
+//             return orderitem;
+//         }
+//         public IEnumerable<OrderItem> DeleteAll(string id)
+//     {
+//         orderitem.Where(orderitem => orderitem.Id == id);
+//         return orderitem;
+//     }
 
-        public List<OrderItem> FindAll(OrderItem orderItem)
-        {
-            throw new NotImplementedException();
-        }
+//         public List<OrderItem> FindAll(OrderItem orderItem)
+//         {
+//             throw new NotImplementedException();
+//         }
 
-        OrderItem IOrderItemRepository.DeleteAll(string id)
-        {
-            throw new NotImplementedException();
-        }
-    } }
-    // rnd
+//         OrderItem IOrderItemRepository.DeleteAll(string id)
+//         {
+//             throw new NotImplementedException();
+//         }
+//     } }
+//     // rnd
